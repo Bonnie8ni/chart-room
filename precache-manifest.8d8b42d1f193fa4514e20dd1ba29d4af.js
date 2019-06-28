2cff8f36c728957101ff3176ee767178bb2d79e2
BonnieYu <Bonnie_Yu@wistron.com>
2019-06-29 02:06:23 +0800

    Updates

---
+++ precache-manifest.8d8b42d1f193fa4514e20dd1ba29d4af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1342feb8ec08b78ac983135e3ec7fc39",
+    "url": "/chat-room/index.html"
+  },
+  {
+    "revision": "99dba2e991ed5b738ba4",
+    "url": "/chat-room/static/js/2.74820e65.chunk.js"
+  },
+  {
+    "revision": "e082161679f4025b3d21",
+    "url": "/chat-room/static/js/main.98a70102.chunk.js"
+  },
+  {
+    "revision": "ec539cdd20229a5c915b",
+    "url": "/chat-room/static/js/runtime~main.70287937.js"
+  }
+]);
